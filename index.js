0c9bb529887d988f0d2e10cd66da8df36731c6fc
Odima-dev <vincentodima22@gmail.com>
2025-02-28 18:05:10 +0300

    selecting html section views

--- index.js
+++ index.js
@@ -9,3 +9,8 @@
 const viewLoggedOut = document.getElementById("logged-out-view")
 const viewLoggedIn = document.getElementById("logged-in-view")
 
+const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
+
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("password-input")
+
