ba62e77da37b6a49ab42d3de896339c70b15d61c
Odima-dev <vincentodima22@gmail.com>
2025-02-28 15:26:11 +0300

    selecting html section views

---
+++ index.js
@@ -0,0 +1,11 @@
+/* === Imports === */
+
+/* === Firebase Setup === */
+
+/* === UI === */
+ 
+/* == UI - Elements == */
+
+const viewLoggedOut = document.getElementById("logged-out-view")
+const viewLoggedIn = document.getElementById("logged-in-view")
+
